d9d1ed120c97b62f0bf7254d1d555325695c6f10
Sinan <goksinan@sabanciuniv.edu>
2017-08-22 18:18:50 +0300

    User model is completed

--- accounts/models.py
+++ accounts/models.py
@@ -4,29 +4,37 @@ from django.utils import timezone
 import datetime
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, email, password=None):
+    def create_user(self, su_id, first_name, last_name, email, phone_number, password=None):
         """
-        Creates and saves a User with the given username, email and password.
+        Creates and saves a User with the given su_id, first_name, last_name, email, phone_number and password.
         """
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(
-            username=username,
+            su_id=su_id,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            phone_number=phone_number,
             email=self.normalize_email(email),
+            password=make_random_password(length=8)
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password):
+    def create_superuser(self, su_id, first_name, last_name, email, phone_number, password):
         """
-        Creates and saves a superuser with the given username, email and password.
+        Creates and saves a superuser with the given su_id, first_name, last_name, email, phone_number and password.
         """
         user = self.create_user(
-            username,
+            su_id,
+            first_name,
+            last_name,
             email,
+            phone_number,
             password=password,
         )
         user.is_admin = True
@@ -35,11 +43,11 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
+
     susail_id = models.AutoField(primary_key=True)
-    username = models.CharField(('username'), max_length=30, blank=True, null=False, unique=True, )
     first_name = models.CharField(('first name'), max_length=30, blank=True, null=False)
     last_name = models.CharField(('last name'), max_length=30, blank=True, null=False)
-    su_id = models.IntegerField(blank=True, null=False, unique=True, )
+    su_id = models.IntegerField(blank=True, null=False, )
     email = models.EmailField(verbose_name='email address', max_length=255, )
 
     # phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
@@ -48,25 +56,34 @@ class User(AbstractBaseUser):
     # django-phonenumber-field 1.1.0 ??
     # https://pypi.python.org/pypi/django-phonenumber-field/1.1.0
 
+    # according to E.164 max. character length of a phone number is 15
+    # localflavor libabry doesn't have PhoneNumberField for Turkey but TRIdentificationNumberField is valid for forms
     phone_number = models.CharField(max_length=15, blank=True, null=False)
     emergency_contact_name = models.CharField(('first name'), max_length=30, blank=True, null=False)
     emergency_phone_number = models.CharField(max_length=15, blank=True, null=False)
-
     date_of_birth = models.DateField(('doğum tarihi'), blank=True, null=True)
-    dept = model.IntegerField(('borç'), blank=True, null=True, )
+    is_entry_fee_paid = models.BooleanField(default=False)
 
     #custom sailing_levelField?
     sailing_level = model.IntegerField(('yelken seviyesi'), blank=True, null=True, )
+    club_management_position = models.CharField(('yönetim kurulu görevi'), max_length=30, blank=True, null=False,)
+    sailing_team_position = models.CharField(('yarış takımı pozisyonu'), max_length=30, blank=True, null=False,)
+    extra_information_about_member = models.TextField(blank=True, null=False,)
+    is_visible_on_web = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(('kayıt tarihi'), auto_now_add=True)
+    is_an_active_student = models.BooleanField(default=True)
+    is_school_staff = models.BooleanField(default=False)
+    is_gorbon_captain = models.BooleanField(default=False)
 
-    yonetim_kurulu_gorevi = models.CharField(('yönetim kurulu görevi'), max_length=30, blank=True, null=False,)
-    yaris_takimi_pozisyonu = models.CharField(('yarış takımı pozisyonu'), max_length=30, blank=True, null=False,)
+    # is_eager_newbie_member?
+    # newbie?
+    # is_aktif_uye?
+    # is_fresh_blood?
+    is_active_member = models.BooleanField(default=False)
 
-    extra_information = models.TextField(blank=True, null=False,)
-
-    disweb = models.BooleanField()
-
-
-    date_joined = models.DateTimeField(('date joined'), auto_now_add=True)
+    # 'kredi' attribute will change to susail_balance
+    # 'borç' attribute is not needed since balance can be both - & +
+    susail_balance = models.IntegerField(blank=True, null=False,)
 
     last_visit = models.DateTimeField(default=timezone.now, blank=True, null=False)
 
@@ -74,15 +91,14 @@ class User(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
-    objects = MyUserManager()
+    objects = UserManager()
 
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['su_id',]
 
     def was_active_recently(self):
         now = timezone.now()
         return self.last_visit >= timezone.now() - datetime.timedelta(minutes=15)
-        was_active_recently.boolean = True
 
     def get_full_name(self):
         # Returns the first_name plus the last_name, with a space in between.
@@ -94,7 +110,7 @@ class User(AbstractBaseUser):
         return self.first_name
 
     def __str__(self):  # __unicode__ on Python 2
-        return self.username
+        return self.get_full_name
 
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
