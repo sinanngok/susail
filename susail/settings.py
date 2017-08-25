96489b4e2aa514efcffbce4eebe2d3f1078e61f0
Sinan <goksinan@sabanciuniv.edu>
2017-08-25 11:34:01 +0300

    syntax correction

--- susail/settings.py
+++ susail/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'accounts'
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
